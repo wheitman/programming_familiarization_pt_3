7d0c70e0a7ef62e68f098e9cd4780f5f026f5751
Will Heitman <16737488+wheitman@users.noreply.github.com>
2024-03-21 18:34:55 -0400

    Initial commit

---
+++ src/motion_decoder/src/motion_decoder.cpp
@@ -0,0 +1,33 @@
+#include <ros/ros.h>
+#include <motion_decoder/image_converter.hpp>
+#include <apriltags_ros/AprilTagDetectionArray.h>
+#include <apriltags_ros/AprilTagDetection.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Quaternion.h>
+
+
+ImageConverter* ic;
+
+void apriltag_detection_callback(const apriltags_ros::AprilTagDetectionArray msg)
+{
+  ROS_INFO("In subscribe\n");
+  //TODO: Parse message and publish transforms as apriltag_tf and camera
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  
+  ros::NodeHandle n;
+  //TODO: Add a subscriber to get the AprilTag detections The callback function skelton is given.
+  ImageConverter converter;
+  ic = &converter;
+  ros::Rate loop_rate(50);
+  ROS_INFO("In main\n");
+  while(ros::ok()) {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
